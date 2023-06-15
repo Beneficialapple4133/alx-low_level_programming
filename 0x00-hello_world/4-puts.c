8cdef7bd6b999cf1bb2119ba9a593d9341c30b8d
Beneficialapple4133 <njeridelorise@gmail.com>
2023-06-15 15:17:45 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
+/**
+ * This is a comment
+ * 
+ */
+
 #include <stdio.h>
-int main (void)
+int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
-	return (0);
-    }
+	return 0;
+}
