f8a68d315afadfe377e1f475e6e6cdbf4df1889b
Beneficialapple4133 <njeridelorise@gmail.com>
2023-06-15 16:19:18 -0700

    Trying again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
- * main - Program starts
+/**
+ * main - Entry point of program
  *
  * Description: Program prints
- * with proper grammar, but the outcome is a piece of art,
- * return - program ends
+ *
+ * "with proper grammar, but the outcome is a piece of art,".
+ *
+ * return: Always 0 and program ends
  */
 int main(void)
 {
