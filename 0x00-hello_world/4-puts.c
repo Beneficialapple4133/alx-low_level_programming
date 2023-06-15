8b9319d9e23a2fc3efd09ea4e7feff4b6d36e912
Beneficialapple4133 <njeridelorise@gmail.com>
2023-06-15 15:40:46 -0700

    4th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-/**
- * This is a comment
- * 
- */
-
 #include <stdio.h>
+/**
+ * main - Entry point of program
+ * Description: This program prints
+ * "Programming is like building a multilingual puzzle
+ * Return: Always 0. program ends here, and successful execution
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
