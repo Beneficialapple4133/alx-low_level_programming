f1df155864d311f8037a0d7578e89ca5df0f0cb1
Beneficialapple4133 <njeridelorise@gmail.com>
2023-06-15 18:52:04 -0700

    Advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * Main - Start of the program
+ *
+ * Description - Program prints 
+ * and that piece of art is useful"
+ * by Dora Korpar
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
