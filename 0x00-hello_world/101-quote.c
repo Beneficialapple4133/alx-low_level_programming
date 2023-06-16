91cf79e28746e683c68492222d929045a2923b8d
Beneficialapple4133 <njeridelorise@gmail.com>
2023-06-15 19:13:15 -0700

    Bro is tired

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,10 @@
  * and that piece of art is useful
  * by Dora Korpar
  *
- * Return 1- program ends abnormally
+ * Return: Is 1- program ends abnormally
  */
 int main(void)
-{
+}
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
