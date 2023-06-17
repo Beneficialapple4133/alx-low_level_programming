cc3af461638c7a9e37b1fb1e2d4e10d1c071f493
Beneficialapple4133 <njeridelorise@gmail.com>
2023-06-16 17:18:19 -0700

    Try

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,13 @@ int main(void)
 	printf("Last digit of 98 is %d and is ", n % 10);
 	if ((n % 10) > 5)
 	{
-		printf("greater than 5\n");
+		printf("Last digit of %d is greater than 5\n");
 	} else if ((n % 10) == 0)
 	{
 		printf("0\n");
 	} else
 	{
-		printf("less than 6 and not 0\n");
+		printf("Last digit of %d is less than 6 and not 0\n");
 	}
 
 	return (0);
