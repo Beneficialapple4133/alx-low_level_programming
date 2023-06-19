357f6d3c88630531b909b5eddae3d1b4ab197f76
Beneficialapple4133 <njeridelorise@gmail.com>
2023-06-18 17:49:58 -0700

    betty is nice today

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main-all combinations of 2 digits
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			putchar('0' + i);
+			putchar('0' + n);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
